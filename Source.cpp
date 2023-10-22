c8540722ab0dc18bdc272ea8b2a76e1856a21a86
SERBAN EC ANDREI <andyspeed2003@gmail.com>
2023-10-22 15:19:02 +0300

    f

--- Source.cpp
+++ Source.cpp
@@ -4,14 +4,13 @@ using namespace std;
 
 
 class Expression {
+private:
+	string expression;
 public:
 	Expression(const string& expression) : expression(expression) {}
 
 	// Metode pentru analiza expresiei și extragerea componentelor
 	// Puteți adăuga aici funcții pentru manipularea expresiilor, cum ar fi extragerea operatorilor și operandelor.
-
-private:
-	string expression;
 };
 
 class ExpressionEvaluator {
@@ -22,15 +21,33 @@ public:
 		// Implementați aici evaluarea expresiei utilizând datele din clasa Expression
 		// Această funcție poate să utilizeze o stivă pentru a gestiona prioritățile și operațiile matematice.
 
+
+
 		return 0.0; // Rezultat temporar
 	}
 };
 
 class Calculator {
+private:
+	bool isRunning;
 public:
+
+	bool getIsRunning()
+	{
+		return this->isRunning;
+	}
+
+	void setIsRunning(bool isRunning)
+	{
+		this->isRunning = isRunning;
+	}
 	Calculator()
 	{
-
+		this->isRunning = true;
+	}
+	Calculator(bool isRunning)
+	{
+		this->isRunning = isRunning;
 	}
 	void run() {
 		ExpressionEvaluator evaluator;
@@ -38,15 +55,14 @@ public:
 
 		cout << "Calculatorul stiintific. Introduceti o expresie sau 'exit' pentru a iesi." << endl;
 
-		while (true) {
+		while (this->isRunning) {
 			cout << "Expresie: ";
 			getline(cin, expression);
 
 			if (expression == "exit") {
-				cout << "La revedere!" << endl;
+				this->isRunning = false;
 				break;
 			}
-
 			double result = evaluator.evaluate(expression);
 			cout << "Rezultat: " << result << endl;
 		}
