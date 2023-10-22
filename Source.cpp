23d323a96d0670f78a46d400b25a9ba7e21c4e1f
SERBAN EC ANDREI <andyspeed2003@gmail.com>
2023-10-22 14:02:08 +0300

    3 classes

--- Source.cpp
+++ Source.cpp
@@ -1,24 +1,61 @@
-#include <iostream>
+﻿#include <iostream>
 #include <string>
 using namespace std;
 
 
-class Expresie
-{
+class Expression {
+public:
+	Expression(const string& expression) : expression(expression) {}
+
+	// Metode pentru analiza expresiei și extragerea componentelor
+	// Puteți adăuga aici funcții pentru manipularea expresiilor, cum ar fi extragerea operatorilor și operandelor.
+
 private:
-	char* input;
+	string expression;
+};
+
+class ExpressionEvaluator {
+public:
+	double evaluate(const string& expression) {
+		Expression expr(expression);
+
+		// Implementați aici evaluarea expresiei utilizând datele din clasa Expression
+		// Această funcție poate să utilizeze o stivă pentru a gestiona prioritățile și operațiile matematice.
+
+		return 0.0; // Rezultat temporar
+	}
+};
+
+class Calculator {
+public:
+	Calculator()
+	{
+
+	}
+	void run() {
+		ExpressionEvaluator evaluator;
+		string expression;
+
+		cout << "Calculatorul stiintific. Introduceti o expresie sau 'exit' pentru a iesi." << endl;
+
+		while (true) {
+			cout << "Expresie: ";
+			getline(cin, expression);
+
+			if (expression == "exit") {
+				cout << "La revedere!" << endl;
+				break;
+			}
+
+			double result = evaluator.evaluate(expression);
+			cout << "Rezultat: " << result << endl;
+		}
+	}
 };
 
 int main()
 {
-	string v;
-	while (cin >> v) 
-	{
-		if (v == "exit")
-		{
-			return 0;
-		}
-		cout << v << endl;
-	}
+	Calculator calc;
+	calc.run();
 }
 
