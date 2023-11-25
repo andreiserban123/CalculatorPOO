7f4f864f7f0279d5ff4b1cb9f5313336b0a5b477
SERBAN EC ANDREI <andyspeed2003@gmail.com>
2023-11-25 18:36:08 +0200

    working with the example

--- Source.cpp
+++ Source.cpp
@@ -3,9 +3,72 @@
 
 using namespace std;
 
-class Parser {
-	char* expression;
+class Evaluator {
+	float result = 0;
 public:
+	Evaluator() {
+		this->result = 0;
+	}
+	Evaluator(float result) {
+		this->result = result;
+	}
+	void evaluate(float operator1, float operator2, char token) {
+		switch (token) {
+		case '+':
+			result = operator1 + operator2;
+			break;
+		case '-':
+			result = operator1 - operator2;
+			break;
+		case '*':
+			result = operator1 * operator2;
+			break;
+		case '/':
+			if (operator2 != 0) {
+				result = operator1 / operator2;
+			}
+			else {
+				cout << "Error: Division by zero." << endl;
+				return;  
+			}
+			break;
+		default:
+			cout << "Error: Unknown operator." << endl;
+			return; 
+		}
+	}
+
+	Evaluator& operator++(int) {
+		Evaluator copie = *this;
+		this->result++;
+		return copie;
+	}
+	float getResult() {
+		return this->result;
+	}
+
+	void printFinalResult()  {
+		cout << "Final Result: " << getResult() << endl;
+	}
+	friend void operator<<(ostream& console, Evaluator& e);
+	friend void operator>>(istream& console, Evaluator& e);
+};
+
+void operator<<(ostream& console, Evaluator& e) {
+	console << e.result << endl;
+}
+void operator>>(istream& console, Evaluator& e) {
+	console >> e.result;
+}
+
+class Parser {
+	char* expression = nullptr;
+	double* results = nullptr;
+public:
+	Parser() {
+		this->expression = nullptr;
+		this->results = nullptr;
+	} 
 	Parser(const string& expression) {
 		string result;
 		for (char c : expression) {
@@ -20,7 +83,45 @@ public:
 	~Parser() {
 		if (this->expression != nullptr)
 			delete[] this->expression;
+		delete[] this->results;
 	}
+
+	void processExpression() {
+		if (this->expression != nullptr) {
+			
+			string expr = this->expression;
+			int startPos, endPos, index;
+			startPos = endPos = index = 0;
+			char oper;
+
+			string vec[100];
+			
+			for (int i = 0; i < expr.size(); i++) {
+				if ((!isdigit(expr[i])) && expr[i]!= '.' ) {
+					oper = expr[i];
+					endPos = i - startPos;  
+					vec[index++] = expr.substr(startPos, endPos);
+					vec[index++] = oper;
+					startPos = i + 1;  
+				}
+			}
+
+			if (startPos < expr.size()) {
+				vec[index++] = expr.substr(startPos);
+			}
+			Evaluator evaluator(stof(vec[0]));
+			for (int i = 1; i < index; i++) {
+				if (!isdigit(vec[i][0])) {
+					
+					float operand2 = stof(vec[i + 1]);
+					char token = vec[i][0];
+					evaluator.evaluate(evaluator.getResult(), operand2, token);
+				}
+			}
+			evaluator.printFinalResult();
+		}
+	}
+
 
 	Parser(const Parser& other) {
 		this->expression = new char[strlen(other.expression) + 1];
@@ -49,22 +150,26 @@ public:
 
 	void setExpression(const string& expr) {
 		if (!expr.empty()) {
+			string result;
+			for (char c : expr) {
+				if (!isspace(c)) {
+					result += c;
+				}
+			}
 			delete[] this->expression;
-			this->expression = new char[expr.length() + 1];
-			strcpy_s(this->expression, expr.length() + 1, expr.c_str());
+			this->expression = new char[result.length() + 1];
+			strcpy_s(this->expression, result.length() + 1, result.c_str());
 		}
 	}
 };
 
-class Evaluator {
-};
 
 class Calculator {
 	bool isRunning = false;
 	static int noCalculators;
 public:
-	Calculator() {
-		isRunning = true;
+	Calculator(){
+		this->isRunning = true;
 		Calculator::noCalculators++;
 	}
 
@@ -75,14 +180,22 @@ public:
 	void setIsRunning(bool isRunning) {
 		this->isRunning = isRunning;
 	}
+
 	~Calculator() {
 		Calculator::noCalculators--;
 	}
 	void run() {
+		Parser p;
 		string expression;
-		cout << "Calculatorul stiintific. Introduceti o expresie sau 'exit' pentru a iesi." << endl;
+		cout << "==============================================" << endl;
+		cout << "       Welcome to the Scientific Calculator    " << endl;
+		cout << "==============================================" << endl;
+		cout << "Instructions: Enter a mathematical expression" << endl;
+		cout << "              Or type 'exit'/'enter' to quit the calculator" << endl;
+		cout << "==============================================" << endl;
+
 		while (this->isRunning) {
-			cout << "Expresie: ";
+			cout << "Expression: ";
 			getline(cin, expression);
 
 			if (expression == "exit" || expression == "") {
@@ -90,9 +203,13 @@ public:
 				break;
 			}
 
-			float result = 0;
-			cout << "Rezultat: " << result << endl;
+			p.setExpression(expression);
+			p.processExpression();
 		}
+	}
+
+	static void printNumberOfCalcs() {
+		cout << "Number of calculators is: " << Calculator::noCalculators;
 	}
 };
 
